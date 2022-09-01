fddf2e63d6c2475e14cb0c6546af0658a3b19897
Kayla S <kaylaesmith1@gmail.com>
2022-09-01 17:08:43 +0000

    Add stock data function to calculate round number of stock to make.

--- run.py
+++ run.py
@@ -135,6 +135,24 @@ def last_five_sales():
     
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate average stock data for each sandwich type and add 10%
+    """
+    print('Calculate stock data')
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        # Since we know the length will always be 5 we could do sum / 5 but if the length varies, good to use lent()
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
+
 def main(): 
     """
     Run all program functions
@@ -144,13 +162,15 @@ def main():
     update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
-    print(new_surplus_data)
+    # print(new_surplus_data)
+    sales_columns = last_five_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 print('Welcome to Love Sandwiches Data Automation')
 # call out main() to test other functions
-# main()
-sales_columns = last_five_sales()
+main()
 
 
 
